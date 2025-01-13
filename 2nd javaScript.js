04fd81f26aa7b9d61e0e0d4652177aae6afdab64
Sudhanshu677 <156607205+Sudhanshu677@users.noreply.github.com>
2025-01-13 21:45:44 +0530

    Add files via upload

---
+++ 2nd javaScript.js
@@ -0,0 +1,21 @@
+function adjustPageScale() {
+    const width = window.innerWidth;
+    let scale = 1;
+  
+    if (width >= 992 && width <= 1600) {
+      scale = 0.9;
+    } else if (width >= 700 && width <= 767) {
+      scale = 0.8;
+    } else if (width >= 600 && width <= 700) {
+      scale = 0.75;
+    } else if (width <= 600) {
+      scale = 0.5;
+    }
+  
+    document.body.style.transform = `scale(${scale})`;
+    document.body.style.transformOrigin = 'top left';
+  }
+  
+  adjustPageScale();
+  window.addEventListener('resize', adjustPageScale);
+  
